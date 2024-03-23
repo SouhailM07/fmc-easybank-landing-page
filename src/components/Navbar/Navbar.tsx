5c605a1b4b1f88ecf76688f46cbff4f5e76472c2
shadow <lightshadow416@gmail.com>
2024-03-23 20:48:22 +0100

    [Navbar] component checkpoint-lg

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,29 @@
 import "./navbar.css";
 // assets
+import logo from "/logo.svg";
 export default function Navbar() {
+  const navLinks: string[] = ["Home", "About", "Contact", "Blog", "Careers"];
   return (
     <>
-      <header>
-        <nav>sb</nav>
+      <header className="py-[0.8rem] px-[3rem] max-w-[80rem] mx-auto sticky top-0">
+        <nav className="flex w-full justify-between items-center">
+          <img src={logo} alt="logo" />
+          <ul
+            role="list"
+            className="flex space-x-[2rem] text-NeutralGrayishBlue "
+          >
+            {navLinks.map((e, i) => {
+              return (
+                <li role="listitem" key={i}>
+                  <a href={`#${e}`}>{e}</a>
+                </li>
+              );
+            })}
+          </ul>
+          <button className=" px-[1.3rem] py-[0.6rem] btn ">
+            Request Invite
+          </button>
+        </nav>
       </header>
     </>
   );
